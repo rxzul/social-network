86375e138e988e08b0bf120cf6f15424636fa212
Timoshenko Danila <dt@nowmedia.ru>
2019-10-03 00:16:04 +0300

    9. Сделан import/export компонент

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Header";
+import Tehnologies from "./Tehnologies";
+
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <Tehnologies />
+      <Tehnologies/>
     </div>
   );
-}
-
-const Header = () => {
-    return (
-        <div className="menu">
-            <a href="">Item 1</a>
-            <a href="">Item 2</a>
-            <a href="">Item 3</a>
-        </div>
-    );
-}
-
-const Tehnologies = () => {
-    return (
-        <ul>
-            <li>html</li>
-            <li>css</li>
-            <li>js</li>
-        </ul>
-    )
 }
 
 export default App;
---
+++ src/Tehnologies.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Tehnologies = () => {
+    return (
+        <ul>
+            <li>html</li>
+            <li>css</li>
+            <li>js</li>
+        </ul>
+    )
+}
+
+export default Tehnologies;
