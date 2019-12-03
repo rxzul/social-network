4c89a65546291993ba6929c4ee20d7c2720d13a9
Timoshenko Danila <dt@nowmedia.ru>
2019-12-03 22:43:52 +0300

    21. Свёрстана страница диалогов

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,29 @@ import classes from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            123
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    Dimych
+                </div>
+                <div className={classes.dialog}>
+                    Andrey
+                </div>
+                <div className={classes.dialog}>
+                    Sveta
+                </div>
+                <div className={classes.dialog}>
+                    Sasha
+                </div>
+                <div className={classes.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>How are you?</div>
+                <div className={classes.message}>Yo!</div>
+            </div>
         </div>
     )
 }
