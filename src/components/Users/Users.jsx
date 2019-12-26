e3ba30defc5827e4f0cf7e57096a657125fc9661
Timoshenko Danila <dt@nowmedia.ru>
2019-12-26 23:42:16 +0300

    51. Добавлена библиотека axios. Сделан запрос к Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import classes from './Users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user-male.png';
 
 const Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: '1', photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/8/88/Dmitry_Nagiev_2017_4.jpg',
-                followed: false, fullName: 'Dmitry', status: 'I am boss', location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: '2', photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/8/88/Dmitry_Nagiev_2017_4.jpg',
-                followed: true, fullName: 'Sasha', status: 'I am boss too', location: {city: 'Moscow', country: 'Russia'}
-            },
-        ]);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items)
+        });
+
+        // props.setUsers([
+        //     {
+        //         id: '1', photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/8/88/Dmitry_Nagiev_2017_4.jpg',
+        //         followed: false, fullName: 'Dmitry', status: 'I am boss', location: {city: 'Minsk', country: 'Belarus'}
+        //     },
+        //     {
+        //         id: '2', photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/8/88/Dmitry_Nagiev_2017_4.jpg',
+        //         followed: true, fullName: 'Sasha', status: 'I am boss too', location: {city: 'Moscow', country: 'Russia'}
+        //     },
+        // ]);
     }
     return (
         <div>
@@ -20,7 +26,7 @@ const Users = (props) => {
                 props.users.map(user => <div key={user.id}>
                     <span>
                         <div>
-                            <img src={user.photoUrl} alt="" className={classes.photo}/>
+                            <img src={ user.photos.small != null ? user.photos.small : userPhoto} alt="" className={classes.photo}/>
                         </div>
                         <div>
                             {
@@ -36,12 +42,12 @@ const Users = (props) => {
                     </span>
                     <span>
                         <span>
-                            <div>{user.fullName}</div>
+                            <div>{user.name}</div>
                             <div>{user.status}</div>
                         </span>
                         <span>
-                            <div>{user.location.country}</div>
-                            <div>{user.location.city}</div>
+                            {/*<div>{user.location.country}</div>*/}
+                            {/*<div>{user.location.city}</div>*/}
                         </span>
                     </span>
                 </div>)
