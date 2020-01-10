d945f8502e524552f49a375dde6869f1d8201d50
Timoshenko Danila <dt@nowmedia.ru>
2020-01-10 22:17:27 +0300

    56. Сделана структура: контейнерная->контейнерная->презентационная компонента

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,41 +1,25 @@
-import React from 'react';
-import classes from './Users.module.css';
-import * as axios from 'axios';
-import userPhoto from '../../assets/images/user-male.png';
+import React from "react";
+import classes from "./Users.module.css";
+import userPhoto from "../../assets/images/user-male.png";
 
-class Users extends React.Component {
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items);
-            this.props.setTotalUsersCount(response.data.totalCount);
-        });
+const Users = (props) => {
+    let pagesCount = Math.ceil (props.totalUsersCount / props.pageSize);
+
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i ++) {
+        pages.push(i);
     }
 
-    onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items)
-        });
-    }
-
-    render() {
-        let pagesCount = Math.ceil (this.props.totalUsersCount / this.props.pageSize);
-
-        let pages = [];
-        for (let i = 1; i <= pagesCount; i ++) {
-            pages.push(i);
-        }
-
-        return (
+    return (
+        <div>
             <div>
-                <div>
-                    {pages.map(p =>  {
-                        return <span className={ (this.props.currentPage === p) && classes.selectedPage}
-                                     onClick={(e) => { this.onPageChanged(p); }}>{p}</span>
-                    })}
-                </div>
-                {
-                    this.props.users.map(user => <div key={user.id}>
+                {pages.map(p =>  {
+                    return <span className={ (props.currentPage === p) && classes.selectedPage}
+                                 onClick={(e) => { props.onPageChanged(p); }}>{p}</span>
+                })}
+            </div>
+            {
+                props.users.map(user => <div key={user.id}>
                     <span>
                         <div>
                             <img src={ user.photos.small != null ? user.photos.small : userPhoto} alt="" className={classes.photo}/>
@@ -44,15 +28,15 @@ class Users extends React.Component {
                             {
                                 user.followed ?
                                     <button onClick={() => {
-                                        this.props.unfollow(user.id)
+                                        props.unfollow(user.id)
                                     }}>Unfollow</button> :
                                     <button onClick={() => {
-                                        this.props.follow(user.id)
+                                        props.follow(user.id)
                                     }}>Follow</button>
                             }
                         </div>
                     </span>
-                        <span>
+                    <span>
                         <span>
                             <div>{user.name}</div>
                             <div>{user.status}</div>
@@ -62,11 +46,10 @@ class Users extends React.Component {
                             {/*<div>{user.location.city}</div>*/}
                         </span>
                     </span>
-                    </div>)
-                }
-            </div>
-        )
-    }
+                </div>)
+            }
+        </div>
+    )
 }
 
 export default Users;
---
+++ src/components/Users/UsersAPIComponent.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import * as axios from 'axios';
+import Users from "./Users";
+
