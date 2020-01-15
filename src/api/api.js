9e40161ed38d63537ff5718ecd03bffa1913b3eb
Timoshenko Danila <dt@nowmedia.ru>
2020-01-16 00:05:43 +0300

    63. Создан Data Access Layer (DAL)

---
+++ src/api/api.js
@@ -0,0 +1,17 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        'API-KEY': '259c9137-229b-4a67-b179-aab98fd9e009'
+    },
+});
+
+export const usersAPI = {
+    getUsers(currentPage, pageSize) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => {
+            return response.data;
+        });
+    }
+}
