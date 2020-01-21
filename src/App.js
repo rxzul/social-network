a2624eef5fc610bb065c5b7deab278983b76a1bf
Timoshenko Danila <dt@nowmedia.ru>
2020-01-21 23:10:15 +0300

    68. Сделан Redirect на логин, если пользователь не зарегистрирован

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Settings from "./components/Settings/Settings";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import LoginPage from "./components/Login/Login";
 
 const App = (props) => {
     return (
@@ -23,6 +24,7 @@ const App = (props) => {
                     <Route path="/news" component={News} />
                     <Route path="/music" component={Music} />
                     <Route path="/settings" component={Settings} />
+                    <Route path="/login" component={LoginPage} />
 
                 </div>
             </div>
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import Profile from "./Profile";
-import * as axios from "axios";
 import {connect} from "react-redux";
 import {getUserProfile} from "../../redux/profile-reducer";
 import {withRouter} from "react-router-dom";
-import {usersAPI} from "../../api/api";
+import Redirect from "react-router-dom/es/Redirect";
 
 class ProfileContainer extends React.Component {
     componentDidMount() {
@@ -17,6 +16,8 @@ class ProfileContainer extends React.Component {
     }
 
     render () {
+        if (!this.props.isAuth) return <Redirect to="/login" />
+
         return (
                 <Profile {...this.props} profile={this.props.profile} />
         )
@@ -24,7 +25,8 @@ class ProfileContainer extends React.Component {
 }
 
 let mapStateToProps = (state) => ({
-    profile: state.profilePage.profile
+    profile: state.profilePage.profile,
+    isAuth: state.auth.isAuth,
 });
 
 let withUrlDataContainerComponent = withRouter(ProfileContainer);
