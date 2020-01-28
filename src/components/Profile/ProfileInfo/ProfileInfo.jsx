b7c7c4a770d88a14b4be01a8a7967ab8325cd265
Timoshenko Danila <dt@nowmedia.ru>
2020-01-28 23:02:47 +0300

    71. Создана компонента для статуса. Сделан локальный state для неё

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
+import ProfileStatus from "./ProfileStatus";
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -9,17 +10,16 @@ const ProfileInfo = (props) => {
 
     return (
         <div>
-            <div>
-                <img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""/>
-            </div>
+            {/*<div>*/}
+            {/*    <img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""/>*/}
+            {/*</div>*/}
             <div className={classes.descriptionBlock}>
                 <img src={props.profile.photos.large} alt=""/>
+                <ProfileStatus status={"Hello!"}/>
                 <h4>{props.profile.aboutMe}</h4>
                 <p>
                     <b>Профиль vk: </b> {props.profile.contacts.vk}
                 </p>
-
-                ava + descr
             </div>
         </div>
     )
