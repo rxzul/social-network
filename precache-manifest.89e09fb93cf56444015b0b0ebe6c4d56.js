f0411ccc09538f16003c747363faeae41e75598e
Timoshenko Danila <dt@nowmedia.ru>
2020-02-24 23:52:19 +0300

    Updates

---
+++ precache-manifest.89e09fb93cf56444015b0b0ebe6c4d56.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e38c367a0b14ecb5edfdaa31b80d214",
+    "url": "/rxzul/social-network/index.html"
+  },
+  {
+    "revision": "ba785dbdb7fe7f885da7",
+    "url": "/rxzul/social-network/static/css/3.d3c72e77.chunk.css"
+  },
+  {
+    "revision": "9442d7a4c269b0c7f6b8",
+    "url": "/rxzul/social-network/static/css/4.314f5c9f.chunk.css"
+  },
+  {
+    "revision": "ca6a7c419524acb17eb2",
+    "url": "/rxzul/social-network/static/css/main.dbb9ad81.chunk.css"
+  },
+  {
+    "revision": "8dc10113a6e56d62a1a5",
+    "url": "/rxzul/social-network/static/js/2.a1a9a407.chunk.js"
+  },
+  {
+    "revision": "ba785dbdb7fe7f885da7",
+    "url": "/rxzul/social-network/static/js/3.72c2cfd2.chunk.js"
+  },
+  {
+    "revision": "9442d7a4c269b0c7f6b8",
+    "url": "/rxzul/social-network/static/js/4.d618d0ec.chunk.js"
+  },
+  {
+    "revision": "ca6a7c419524acb17eb2",
+    "url": "/rxzul/social-network/static/js/main.cbf4db85.chunk.js"
+  },
+  {
+    "revision": "b621be1e829870fe6922",
+    "url": "/rxzul/social-network/static/js/runtime~main.e985f6af.js"
+  },
+  {
+    "revision": "a97be9d162ddf593523f839995926fee",
+    "url": "/rxzul/social-network/static/media/preloader.a97be9d1.svg"
+  }
+]);
