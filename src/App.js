7fc25326af5528e164a7894cd20a6d4621faa1b1
Timoshenko Danila <dt@nowmedia.ru>
2020-02-24 23:25:15 +0300

    94. Добавлены React.lazy и React.Suspense

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ import {BrowserRouter, Route, withRouter} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import LoginPage from "./components/Login/Login";
 import {connect, Provider} from "react-redux";
 import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
 import store from "./redux/redux-store";
+import {withSuspense} from "./hoc/withSuspense";
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 class App extends React.Component {
 
@@ -32,9 +34,9 @@ class App extends React.Component {
                 <HeaderComponent/>
                 <Navbar/>
                 <div className="app-wrapper-content">
-                    <Route path="/dialogs" render={() => <DialogsContainer/>}/>
+                    <Route path="/dialogs" render={withSuspense(DialogsContainer)}/>
                     {/*? - означает, что параметр не обязательный*/}
-                    <Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
+                    <Route path="/profile/:userId?" render={withSuspense(ProfileContainer)}/>
                     <Route path="/users" render={() => <UsersContainer/>}/>
                     <Route path="/news" component={News}/>
                     <Route path="/music" component={Music}/>
